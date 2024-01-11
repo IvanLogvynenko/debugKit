e49c7063650254e26ed7888c23943f45867d7904
Ivan Logvynenko <93791126+21whitereaper12@users.noreply.github.com>
2024-01-11 11:28:20 +0100

    created an include from git hub file for cmake

--- debugKig.cmake
+++ debugKig.cmake
@@ -1,1 +1,12 @@
 include(FetchContent)
+FetchContent_Declare(
+        debugKit
+        GIT_REPOSITORY https://github.com/IvanLogvynenko/debugKit
+)
+FetchContent_GetProperties(debugKit)
+if (NOT azure-storage-lite_POPULATED)
+    FetchContent_Populate(debugKit)
+    add_subdirectory(${debugKit_SOURCE_DIR} ${debugKit_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif ()
+target_compile_options(debugKit PRIVATE -w)
+target_include_directories(debugKit SYSTEM INTERFACE ${debugKit_SOURCE_DIR}/include/)
