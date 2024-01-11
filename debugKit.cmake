4e286a728e478b32bc50a577de9a02930de8d56c
Ivan Logvynenko <93791126+21whitereaper12@users.noreply.github.com>
2024-01-11 12:23:47 +0100

    fixed debugKit.cmake

--- debugKit.cmake
+++ debugKit.cmake
@@ -4,10 +4,6 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/IvanLogvynenko/debugKit
         GIT_TAG 8f106e29565acbeb5fa417e68cf9d919d0e991d2 # release 0.0.1.1
 )
-FetchContent_GetProperties(debugKit)
-if (NOT debugKit_POPULATED)
-    FetchContent_Populate(debugKit)
-    add_subdirectory(${debugKit_SOURCE_DIR} ${debugKit_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif ()
+FetchContent_MakeAvailable(debugKit)
 target_compile_options(debugKit PRIVATE -w)
 target_include_directories(debugKit SYSTEM INTERFACE ${debugKit_SOURCE_DIR}/include/)
