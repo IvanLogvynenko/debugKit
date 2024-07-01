7478f96aa0f5886f557d176e112cc319da6a91f4
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-07-01 11:38:06 +0200

    updated Git tag in cmake install file

--- debugKit.cmake
+++ debugKit.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
         debugKit
         GIT_REPOSITORY https://github.com/IvanLogvynenko/debugKit
-        GIT_TAG 6a9ce4aa3b271a66594d6221f931bb37c7744f2e # release 0.0.1.1
+        GIT_TAG 642a36047bce4ca5f6869da5715d1eacd821fc14 # release 0.0.1.1
 )
 FetchContent_MakeAvailable(debugKit)
 target_compile_options(debugKit PRIVATE -w)
