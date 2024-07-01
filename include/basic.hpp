3ebfcc182ab61b9df72fc5a82f61f6c351e86cf9
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-07-01 12:10:48 +0200

    update logging to match the standart

--- include/basic.hpp
+++ include/basic.hpp
@@ -5,19 +5,19 @@
 #endif // !EXECUTABLE_NAME
 
 #ifndef RELEASE
-    #define ILOG(x) std::cout << ">>> " << EXECUTABLE_NAME << " >>> " << x << "\n"; 
+    #define ILOG(x) std::cout << " [ IMPORTANT ] [ " << EXECUTABLE_NAME << " ] : " << x << "\n"; 
 #else
     #define ILOG(x)
 #endif // DEBUG 
 
 #ifdef FULL_LOGGING
-    #define  LOG(x) std::cout << "    " << EXECUTABLE_NAME << "  >  " << x << "\n"; 
+    #define  LOG(x) std::cout << "               [ " << EXECUTABLE_NAME << " ] : " << x << "\n"; 
 #else
     #define LOG(x)
 #endif // FULL_LOGGING
 
 #ifdef ERROR
-    #define  EL(x) std::cerr << ">>> " << EXECUTABLE_NAME << " >>> " << x << "\n"; 
+    #define  EL(x) std::cerr << "  [   ERROR   ] [ " << EXECUTABLE_NAME << " ] : " << x << "\n"; 
 #else
     #define EL(x)
 #endif // ERROR
