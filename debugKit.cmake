a89b8d52dd97dae39bdac9ad5e10bd81b97e2609
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-07-01 12:14:07 +0200

    update include file for CMake

--- debugKit.cmake
+++ debugKit.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
         debugKit
         GIT_REPOSITORY https://github.com/IvanLogvynenko/debugKit
-        GIT_TAG 642a36047bce4ca5f6869da5715d1eacd821fc14 # release 0.0.1.1
+        GIT_TAG 3ebfcc182ab61b9df72fc5a82f61f6c351e86cf9 # release 0.0.1.1
 )
 FetchContent_MakeAvailable(debugKit)
 target_compile_options(debugKit PRIVATE -w)
