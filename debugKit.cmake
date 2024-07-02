79dd81efc3cc5c5e9e29265945ddcdace92541af
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-07-02 11:00:13 +0200

    update CMakeLists to resolve an issue when the library is used as a dependency

--- debugKit.cmake
+++ debugKit.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
         debugKit
         GIT_REPOSITORY https://github.com/IvanLogvynenko/debugKit
-        GIT_TAG 3ebfcc182ab61b9df72fc5a82f61f6c351e86cf9 # release 0.0.1.1
+        GIT_TAG main
 )
 FetchContent_MakeAvailable(debugKit)
 target_compile_options(debugKit PRIVATE -w)
