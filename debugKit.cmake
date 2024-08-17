7765b981f04c692b474a9b9228fd91ec4ddcd7d0
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-08-17 03:18:17 +0200

    clean cmakelists from test target and remove .vscode)

--- debugKit.cmake
+++ debugKit.cmake
@@ -7,3 +7,4 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(debugKit)
 target_compile_options(debugKit PRIVATE -w)
 target_include_directories(debugKit SYSTEM INTERFACE ${debugKit_SOURCE_DIR}/include/)
+target_compile_definitions(debugKit PRIVATE EXECUTABLE_NAME="Set your name in debugKit.cmake")
